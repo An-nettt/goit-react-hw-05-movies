ef9b5db2b9611765eb9361e0b39caf782a68d3a7
Anna Kravets <annakravets1906@gmail.com>
2023-05-26 07:56:26 +0300

    Added a routing skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import Movies from '../pages/Movies';
+import Films from '../pages/Films';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<Films />} />
+      </Routes>
     </div>
   );
 };
