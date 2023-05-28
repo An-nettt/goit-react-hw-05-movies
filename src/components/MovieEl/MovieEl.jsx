52554bcc2b8ef1ebc9e60b5ad8303440471110cd
Anna Kravets <annakravets1906@gmail.com>
2023-05-28 17:14:39 +0300

    Update MovieEl.jsx

--- src/components/MovieEl/MovieEl.jsx
+++ src/components/MovieEl/MovieEl.jsx
@@ -1,12 +1,14 @@
 // import { Container } from './HomeStyled';
 
 const MovieEl = ({ movie }) => {
-  // const { id, original_title, overview, genres } = movie;
+  const { id, original_title, overview } = movie;
   return (
-    <div key={movie.id}>
-      <h1>{movie.original_title}</h1>
-      <p>{movie.overview}</p>
-      <p>{movie.genres}</p>
+    <div key={id}>
+      <h1>{original_title}</h1>
+      <h2>Overview</h2>
+      <p>{overview}</p>
+      <h2>Genres</h2>
+      {/* <p>{movie.genres}</p> */}
     </div>
   );
 };
